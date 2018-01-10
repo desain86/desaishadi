b22fcf231999531e51c0296432fb0fce7110924a
Nihar Desai <desain86@gmail.com>
2018-01-10 05:29:43 -0800

    Added arrow navigation and mouse control to carousel slider. Change delay and speed of carousel slider.

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,11 @@
             /* options are 'auto', 'fullwidth' or 'fullscreen' */
             sliderLayout: 'fullscreen',
 
-            
+            delay: 3400,
+
+            autoHeight: 'on',
+
+            disableProgressBar:"on",
 
             sliderType: 'carousel',
  
@@ -25,20 +29,61 @@
 		        space: -290,
 		        infinity: 'on',
 		        stretch: 'off',
-		 
-		        border_radius: '10px',
+		 		speed: 1500,
+		        
 		        horizontal_align: 'center',
 		 
 		        fadeout: 'on',
 		        vary_fade: 'on',
 		 
 		        vary_rotation: 'off',
-		        maxRotation: 0,
+		        maxRotation: 180,
 		 
 		        vary_scale: 'off',
 		        minScale: 22
 		 
 		    },
+
+		    navigation: {
+ 
+    			keyboardNavigation: 'on',
+    			keyboard_direction: 'horizontal',
+    			mouseScrollNavigation: 'on',
+    			mouseScrollReverse: 'reverse',
+    			onHoverStop: 'off',
+
+    			arrows: {
+			 
+			        enable: true,
+			        style: 'uranus',
+			        tmp: '',
+			        rtl: false,
+			        hide_onleave: false,
+			        hide_onmobile: true,
+			        hide_under: 0,
+			        hide_over: 9999,
+			        hide_delay: 200,
+			        hide_delay_mobile: 1200,
+			 
+			        left: {
+			            container: 'slider',
+			            h_align: 'left',
+			            v_align: 'center',
+			            h_offset: 20,
+			            v_offset: 0
+			        },
+			 
+			        right: {
+			            container: 'slider',
+			            h_align: 'right',
+			            v_align: 'center',
+			            h_offset: 20,
+			            v_offset: 0
+			        }
+			 
+			    }
+ 
+			},
  
 
         });
